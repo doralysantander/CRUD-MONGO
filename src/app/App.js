584ad10d6c5d008916ee5bccd793d41ea7f339dd
doraly <doraly9110@hotmail.com>
2022-04-05 15:42:33 -0500

    agregando cambios frontend

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,69 @@
 import React, { Component } from 'react';
 //componente de toda la aplicacion
 class App extends Component{
+    constructor(){
+        super();
+        //cuando comience la aplicacion los datos en blanco
+        this.state = {
+            title: '',
+            description: ''
+        };
+        this.addTask = this.addTask.bind(this);
+    }
+    addTask(e){
+        console.log(this.state);
+        e.preventDefault();
+    }
     render(){
         return(
-            <h1>Hola Mundo</h1>
+            <div>
+                {/* NAVEGACION*/}
+                <nav className="light-blue darken-4">
+                    <div className="container">
+                        <a className="brand-logo" href="/">MERN Stack</a>
+                    </div>
+                </nav>
+                <div className="container">
+                    <div className="row">
+                        <div className="col s5">
+                          <div className="card">
+                              <div className="card-content">
+                                  <form onSubmit={this.addTask}>
+                                      < div className="row">
+                                          <div className="input-field col s12">
+                                              <input type="text" placeholder="Task title"/>
+
+                                          </div>
+
+                                      </div>
+
+                                      < div className="row">
+                                          <div className="input-field col s12">
+                                              <textarea placeholder="Task Description" className="materialize-textarea"></textarea>
+
+                                          </div>
+
+                                      </div>
+                                      <button  type="submit"className="btn light-blue darken-4">
+                                          send
+                                      </button>
+                                  </form>
+
+                              </div>
+
+                          </div>
+                
+
+                        </div>
+                        <div className="col s7">
+
+                        </div>
+
+                    </div>
+                    
+
+                </div>
+            </div>
         )
     }
 }
