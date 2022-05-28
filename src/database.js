87d01852344527f42d8d93f615ca0dbf3e691c86
doraly <doraly9110@hotmail.com>
2022-05-28 13:13:23 -0500

    cambiando la ruta de mongoatlas

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 //const URI = 'mongodb://localhost/BDPRUEBA'
-const URI = 'mongodb+srv://doraly:bd123@cluster0.23cnr.mongodb.net/bdtodo?retryWrites=true&w=majority';
+//const URI = 'mongodb+srv://doraly:bd123@cluster0.23cnr.mongodb.net/bdtodo?retryWrites=true&w=majority';
+const URI='mongodb+srv://doraly:doraly@cluster0.23cnr.mongodb.net/?retryWrites=true&w=majority'
 mongoose.connect(URI)
 .then(db => console.log('BD is connected'))
 .catch(err => console.error(err));
